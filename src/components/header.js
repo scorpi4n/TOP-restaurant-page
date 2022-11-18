a60ec984cbaf0d2d849ecacdb8ad2211a2d85db7
James Phillips <scorpi4n@scorpi4n.com>
2022-11-18 00:32:45 -0600

    Add flex utility class

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 export default function () {
 	const header = document.createElement('header')
+	header.classList.add('flex')
 	header.innerHTML = `
 		<h1>Restaurant</h1>
-		<ul>
+		<ul class="flex">
 			<li>Home</li>
 			<li>Menu</li>
 			<li>About</li>
