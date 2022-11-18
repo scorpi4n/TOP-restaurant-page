31b94740efaf09eb3579f8de105e720283272e03
James Phillips <scorpi4n@scorpi4n.com>
2022-11-17 23:53:46 -0600

    Create template and entry for webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import './reset.css';
-import './style.css';
+import './styles/reset.css';
+import './styles/style.css';
+
+import header from './components/header';
+import menu from './pages/menu';
+
+const content = document.getElementById('content')
+
+content.appendChild(header())
+content.appendChild(menu())
