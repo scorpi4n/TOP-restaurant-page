da467698dc97aa4cf65f3e57edd6013be70ae368
James Phillips <scorpi4n@scorpi4n.com>
2022-12-02 02:05:37 -0600

    Update hero styles

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,16 +4,18 @@ export default function () {
 			<div class="hero flex">
 				<h1>Delicious Food</h1>
 				<p>Classy food for classy people or something</p>
-				<button class="cta">Menu <img src="#" alt=""></img></button>
+				<button class="cta">Menu <img src="#" alt=""/></button>
+				<img class="hero-image" src="/src/assets/beef.jpg" aria-hidden="true"/>
+				<div class="hero-gradient" aria-hidden="true"></div>
 			</div>
-			<div class="selling-point flex">
-				<div class="flex">
-					<h2>Lorem Ipsum</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					</div>
-					<img src="/src/assets/beef.jpg" alt=""></img>
-			</div>
-	`
+			`
+	// <div class="selling-point flex">
+	// 	<div class="flex">
+	// 		<h2>Lorem Ipsum</h2>
+	// 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+	// 		</div>
+	// 		<img src="/src/assets/beef.jpg" alt=""></img>
+	// </div>
 
 	return home
 }
