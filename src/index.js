f7850238eefaa41beff7119d51c7ad0e38a3bcda
James Phillips <scorpi4n@scorpi4n.com>
2022-12-06 19:31:59 -0600

    Create render and unrender functions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
 import './styles/reset.css';
 import './styles/style.css';
 
+import home from './pages/home';
 import menu from './pages/menu';
+import about from './pages/about'
 import pageLoad from './pageLoad';
+import header from './components/header';
+import footer from './components/footer';
 
 const content = document.getElementById('content')
 
 // load the page contents into #content
 pageLoad()
 
+export function unrender() {
+	content.innerHTML = ""
+}
+
+export function render(slot) {
+	content.appendChild(header())
+	content.appendChild(slot())
+	content.appendChild(footer())
+
+	document.getElementById('home-btn').onclick = function () {
+		unrender()
+		render(home)
+	}
+
+	document.getElementById('menu-btn').onclick = function () {
+		unrender()
+		render(menu)
+	}
+
+	document.getElementById('about-btn').onclick = function () {
+		unrender()
+		render(about)
+	}
+}
+
 // add eventlisteners to nav buttons
 document.getElementById('home-btn').onclick = function () {
-	content.appendChild(home())
+	unrender()
+	render(home)
 }
 
 document.getElementById('menu-btn').onclick = function () {
-	content.appendChild(menu())
+	unrender()
+	render(menu)
 }
 
 document.getElementById('about-btn').onclick = function () {
-	content.appendChild(about())
+	unrender()
+	render(about)
 }
