d9e8182d8f52eb4bcfd3d3fd78e34058a631ad01
James Phillips <scorpi4n@scorpi4n.com>
2022-12-08 01:48:21 -0600

    Extract contents into Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+export default function () {
+	const Hero = document.createElement('div')
+	Hero.classList.add('hero')
+	Hero.innerHTML = `
+		<img class="hero-image" src="/src/assets/beef.jpg" aria-hidden="true"/>
+		<div class="hero-gradient" aria-hidden="true"></div>
+		<div class="hero-content flex">
+			<p class="title">Delicious Food</p>
+			<p>Classy food for classy people or something</p>
+			<a class="cta">Check out the Menu</a>
+			<a href="#">
+				<img alt="Down arrow" class="hero-scroll" src="/src/assets/expand-down.png" />
+			</a>
+		</div>
+	`
+
+	return Hero
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,8 @@
+import Hero from "../components/Hero"
+
 export default function () {
 	const home = document.createElement('div')
-	home.innerHTML = `
-			<img class="hero-image" src="/src/assets/beef.jpg" aria-hidden="true"/>
-			<div class="hero-gradient" aria-hidden="true"></div>
-			<div class="hero flex">
-				<h1>Delicious Food</h1>
-				<p>Classy food for classy people or something</p>
-				<button class="cta">Menu <img src="#" alt=""/></button>
-			</div>
-			`
+	home.appendChild(Hero())
 
 	return home
 }
