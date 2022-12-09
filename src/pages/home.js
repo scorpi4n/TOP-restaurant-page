f8e4fcc851561c5c01469a54bcaff190f8dcd2ec
James Phillips <scorpi4n@scorpi4n.com>
2022-12-08 21:46:35 -0600

    Plan out home page in comments

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,12 @@
 import Hero from "../components/Hero"
+import SocialProof from "../components/SocialProof"
 
 export default function () {
 	const home = document.createElement('div')
 	home.appendChild(Hero())
+	home.appendChild(SocialProof())
+	// menu preview
+	// leave a review
 
 	return home
 }
