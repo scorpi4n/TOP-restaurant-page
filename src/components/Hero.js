6abf2362da7392ca6960d7e7c9d9be8a324aab76
James Phillips <scorpi4n@scorpi4n.com>
2022-12-20 17:38:45 -0600

    Move gradient into css ::after

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,6 @@ export default function () {
 	Hero.classList.add('hero')
 	Hero.innerHTML = `
 		<img class="hero-image" src="/src/assets/beef.jpg" aria-hidden="true"/>
-		<div class="hero-gradient" aria-hidden="true"></div>
 		<div class="hero-content flex">
 			<p class="title">Delicious Food</p>
 			<p>Classy food for classy people or something</p>
