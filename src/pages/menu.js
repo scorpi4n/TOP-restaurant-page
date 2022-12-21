9bf7140f6b5aad82c50d2dbd0022fae16b8b376e
James Phillips <scorpi4n@scorpi4n.com>
2022-12-21 12:20:03 -0600

    Add title to menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -4,11 +4,20 @@ import foodItems from '../data/foodItems.js';
 export default function () {
 	const menu = document.createElement('div')
 	menu.id = 'menu'
-	menu.classList.add('grid')
+
+	const title = document.createElement('h1')
+	title.classList.add("title")
+	title.innerText = "Menu"
+
+	const menuList = document.createElement('ul')
+	menuList.classList.add("grid", "menu")
 
 	foodItems.forEach(item => {
-		menu.appendChild(MenuItem(item))
+		menuList.appendChild(MenuItem(item))
 	})
+
+	menu.appendChild(title)
+	menu.appendChild(menuList)
 
 	return menu
 }
