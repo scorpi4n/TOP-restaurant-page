f8ee5afefa73b47498a155dd000a0b6770f85de7
James Phillips <scorpi4n@scorpi4n.com>
2022-12-21 20:06:09 -0600

    Update hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ export default function () {
 	Hero.classList.add('hero')
 	Hero.innerHTML = `
 		<img class="hero-image" src="/src/assets/beef.jpg" aria-hidden="true"/>
-		<div class="hero-content">
+		<div class="hero-content flex">
 			<p class="title">Delicious Food</p>
 			<p>Classy food for classy people or something</p>
 			<a class="cta">Check out the Menu</a>
