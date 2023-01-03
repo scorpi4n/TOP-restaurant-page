139e69a3ab1a5012c970a73cb3ea32c8075e4aa1
James Phillips <scorpi4n@scorpi4n.com>
2023-01-02 20:31:49 -0600

    Create menu preview for home page

---
+++ src/components/MenuPreview.js
@@ -0,0 +1,22 @@
+import MenuItem from "../components/MenuItem"
+import foodItems from "../data/foodItems"
+
+export default function () {
+	const MenuPreview = document.createElement('article')
+	MenuPreview.classList.add('menu-preview')
+
+	const arrayEl = document.createElement('div')
+	arrayEl.classList.add('grid')
+	for (let i = 0; i < 3; i++) {
+		arrayEl.appendChild(MenuItem(foodItems[i]))
+	}
+	MenuPreview.appendChild(arrayEl)
+
+	const cta = document.createElement('button')
+	cta.classList.add('cta', 'flex')
+	cta.innerText = "Check out the menu!"
+	MenuPreview.appendChild(cta)
+
+
+	return MenuPreview
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,12 @@
 import Hero from "../components/Hero"
 import SocialProof from "../components/SocialProof"
+import MenuPreview from "../components/MenuPreview"
 
 export default function () {
 	const home = document.createElement('div')
 	home.appendChild(Hero())
 	home.appendChild(SocialProof())
-	// menu preview
+	home.appendChild(MenuPreview())
 	// leave a review
 
 	return home
