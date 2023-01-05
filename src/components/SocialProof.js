fff276c47a78b104e8d3c730f466e2b73a93bdd6
James Phillips <scorpi4n@scorpi4n.com>
2023-01-04 22:50:44 -0600

    Update social proof title

--- src/components/SocialProof.js
+++ src/components/SocialProof.js
@@ -3,7 +3,7 @@ export default function () {
 	SocialProof.classList.add("social-proof");
 	SocialProof.id = "social-proof";
 	SocialProof.innerHTML = `
-		<h2>From the local news</h2>
+		<h2 class="title">From the local news</h2>
 		<blockquote cite="https://www.loremipsum.io">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque elit eget gravida cum sociis natoque penatibus. Justo laoreet sit amet cursus sit amet dictum sit. Eros donec ac odio tempor orci dapibus ultrices. Velit ut tortor pretium viverra suspendisse potenti nullam ac. Accumsan lacus vel facilisis volutpat est. Dictumst quisque sagittis purus sit. Enim nulla aliquet porttitor lacus luctus accumsan tortor. Proin fermentum leo vel orci. In tellus integer feugiat scelerisque varius morbi enim. Varius quam quisque id diam vel quam elementum pulvinar etiam. Sit amet est placerat in. Enim tortor at auctor urna nunc id cursus metus. Sed arcu non odio euismod lacinia at quis risus. Turpis egestas maecenas pharetra convallis posuere. Nisi quis eleifend quam adipiscing vitae. Arcu non odio euismod lacinia at quis risus sed vulputate. Sed augue lacus viverra vitae congue eu consequat ac.</blockquote>
 	`;
 
